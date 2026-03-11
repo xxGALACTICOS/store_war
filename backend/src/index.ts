83f1176fdea4d164fe546b3acfebaa37d1bcaed8
ebrahem648 <ebrahemmohamed1237@gmail.com>
2026-03-11 15:39:05 +0200

    hima bymassy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import type { Application } from "express";
 import cors from "cors";
 
 import { config } from "./config/config.ts";
+
 /*
  * CUSTOM IMPORTS
  */
@@ -10,6 +11,14 @@ import { config } from "./config/config.ts";
 const app: Application = express();
 
 app.use(express.json());
+app.get("/products/hamdy", function (req, res) {
+  const product: any = {
+    name: "bassam",
+    rate: 3,
+    price: 500,
+  };
+  res.json(product);
+});
 app.use(
   cors({
     origin: "http://localhost:5173",
