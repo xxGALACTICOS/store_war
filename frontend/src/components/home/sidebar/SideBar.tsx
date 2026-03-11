ae6029aeffeeb5013bab5832627c653b6681d541
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-03-11 16:14:54 +0200

    style(sidebar): fixed height of sidebar

--- frontend/src/components/home/sidebar/SideBar.tsx
+++ frontend/src/components/home/sidebar/SideBar.tsx
@@ -14,7 +14,7 @@ const data = [
 ////////////////////////////////////////////////////
 const SideBar = () => {
     return (
-        <div className="w-[15%] bg-gradient-to-b from-[#011c40] to-[#014e7c] p-3 text-white hidden md:block">
+        <div className="min-h-screen w-[15%] bg-gradient-to-b from-[#011c40] to-[#014e7c] p-3 text-white hidden md:block">
             <p className="font-bold text-lg mb-3">Category</p>
             {data.map((category) => {
                 return <p className="text-sm text-gray-400 hover:underline cursor-pointer pb-1"> {category} </p>
