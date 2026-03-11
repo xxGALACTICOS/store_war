5f976d557262e8d069402a8c24c3ab9f30e2580e
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-03-11 16:43:33 +0200

    feat(navbar): added sign in button and checks for authentication

--- frontend/src/components/home/navbar/NavBar.tsx
+++ frontend/src/components/home/navbar/NavBar.tsx
@@ -3,6 +3,12 @@ import DefPfp from '../../../../assets/Default_pfp.svg'
 import { Link } from 'react-router'
 import SearchBar from './SearchBar'
 import { HeartIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
+import { UserIcon } from '@heroicons/react/16/solid'
+
+/////////////////////////////////////////
+const isAuthenticated = 0;
+/////////////////////////////////////////
+
 const NavBar = () => {
     return (
         <nav className='p-2 flex justify-between bg-gradient-to-r from-[#011c40] to-[#014e7c] w-full sticky top-0 z-10'>
@@ -19,7 +25,12 @@ const NavBar = () => {
                 <Link to='/cart'>
                     <ShoppingCartIcon color='white' className='size-10 cursor-pointer' />
                 </Link>
-                <img src={DefPfp} alt="Profile" className="cursor-pointer rounded-full w-15 h-15" />
+                {isAuthenticated ? <img src={DefPfp} alt="Profile" className="cursor-pointer rounded-full w-15 h-15" /> :
+                    <button className='flex items-center gap-1.5 bg-gradient-to-r from-[#014e7c] to-[#011c40] hover:from-[#015f96] hover:to-[#012850] text-white text-xs font-semibold px-3 py-1.5 rounded-full shadow-sm transition-all duration-200 active:scale-100 mr-1 cursor-pointer scale-120'>
+                        <UserIcon className='size-3.5 white' />
+                        Sign in
+                    </button>}
+
             </div>
         </nav>
     )
