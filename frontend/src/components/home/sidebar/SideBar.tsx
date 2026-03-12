c5930159d8d8051ae4275709fdac1e65cf74d701
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-03-12 11:03:23 +0200

    feat(sidebar): added children of the categories and handled endpoints

--- frontend/src/components/home/sidebar/SideBar.tsx
+++ frontend/src/components/home/sidebar/SideBar.tsx
@@ -1,23 +1,51 @@
+import { ChevronRightIcon } from "@heroicons/react/16/solid"
+import { useState } from "react"
+import { Link } from "react-router"
+
 //////////////////////////////////////////////////
 const data = [
-    "Electronics",
-    "Clothing",
-    "Home & Kitchen",
-    "Sports & Outdoors",
-    "Books",
-    "Beauty & Personal Care",
-    "Toys & Games",
-    "Automotive",
-    "Health & Wellness",
-    "Food & Groceries",
+    { name: "Electronics", children: ["Mobile Phones", "Laptops", "Tablets", "Cameras", "Headphones"] },
+    { name: "Clothing", children: ["Men", "Women", "Kids", "Shoes", "Accessories"] },
+    { name: "Home & Kitchen", children: ["Furniture", "Cookware", "Bedding", "Lighting", "Storage"] },
+    { name: "Sports & Outdoors", children: ["Fitness", "Camping", "Cycling", "Swimming", "Team Sports"] },
+    { name: "Books", children: ["Fiction", "Non-Fiction", "Science", "History", "Comics"] },
+    { name: "Beauty & Personal Care", children: ["Skincare", "Haircare", "Makeup", "Fragrances", "Tools"] },
+    { name: "Toys & Games", children: ["Board Games", "Action Figures", "Puzzles", "Outdoor Toys", "Educational"] },
+    { name: "Automotive", children: ["Car Parts", "Tools", "Accessories", "Oils & Fluids", "Electronics"] },
+    { name: "Health & Wellness", children: ["Vitamins", "Medical Devices", "Personal Care", "Diet", "Mental Health"] },
+    { name: "Food & Groceries", children: ["Snacks", "Beverages", "Dairy", "Bakery", "Organic"] },
 ]
 ////////////////////////////////////////////////////
 const SideBar = () => {
+    const [openCategory, setOpenCategory] = useState([''])
+    const CategoryToggle = (name: string) => {
+        setOpenCategory((arr) =>
+            arr.includes(name) ? arr.filter((category) => category !== name) : [...arr, name]
+        );
+    }
     return (
         <div className="min-h-screen w-[15%] bg-gradient-to-b from-[#011c40] to-[#014e7c] p-3 text-white hidden md:block">
             <p className="font-bold text-lg mb-3">Category</p>
             {data.map((category) => {
-                return <p className="text-sm text-gray-400 hover:underline cursor-pointer pb-1"> {category} </p>
+                const isOpened = openCategory.includes(category.name)
+                return <div className="text-sm text-gray-400 pb-1  transition-all duration-200 ">
+                    <div className="flex select-none hover:text-gray-300" onClick={() => CategoryToggle(category.name)}>
+                        <p> {category.name} </p>
+                        <ChevronRightIcon
+                            className={`size-5 transition-transform duration-300 ${isOpened ? "rotate-90" : ""}`}
+                        />
+                    </div>
+                    <div className={`overflow-hidden transition-all duration-300 ${isOpened ? "max-h-50" : "max-h-0"
+                        }`}>
+                        {isOpened && category.children.map((name) => {
+                            const endpoint = category.name.toLowerCase().replace(/\s+/g, '-') + '/' + name.toLowerCase().replace(/\s+/g, '-')
+                            return <Link to={endpoint}>
+                                <p className="ml-5 text-gray-300 cursor-pointer hover:underline ">{name}</p>
+                            </Link>
+                        })}
+                    </div>
+                </div>
+
             })}
         </div>
     )
