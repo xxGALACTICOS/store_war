80dc06d23beb4c8ec8b7af9a87f415331592811c
pc <ahmedkhalx1221@gmail.com>
2026-03-16 05:52:07 +0200

    handel the 5ara

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import HomePage from './pages/HomePage'
-<<<<<<< Updated upstream
 import { ToastContainer } from "react-toastify"
 import Log from './components/home/logIn/Log'
 import SignUp from './components/home/logIn/SignUp'
-=======
 import ForgetPassword from './pages/ForgetPassword'
 import { Otp } from './pages/Otp'
->>>>>>> Stashed changes
 function App() {
 
   return (
@@ -17,14 +14,10 @@ function App() {
         <Routes>
           <Route path='/' element={<div className='flex bg-red-500 justify-center items-center h-screen'> TEST </div>} />
           <Route path='/home' element={<HomePage />} />
-<<<<<<< Updated upstream
           <Route path='/login' element={<Log />} />
           <Route path='/signup' element={<SignUp />} />
-
-=======
           <Route path='/forget' element={<ForgetPassword />} />
           <Route path='/otp' element={<Otp />} />
->>>>>>> Stashed changes
         </Routes>
       </Router>
     </div>
