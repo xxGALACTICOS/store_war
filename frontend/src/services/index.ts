d3c27222864b702528062e810cd5581ba284a595
ahmedhamdiola <ahmedhamdyua07@gmail.com>
2026-03-19 11:17:11 +0200

    chore: used axios in services instead of regular fetch

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,6 +1,6 @@
+import axios from 'axios'
 const BASE_URL = 'http://localhost:6969'
 export const getProducts = async () => {
-    const res = await fetch(`${BASE_URL}/products/hamdy`)
-    const data = await res.json()
-    return data
+    const res = await axios.get(`${BASE_URL}/products/hamdy`)
+    return res.data
 }
